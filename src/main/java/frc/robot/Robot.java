6686c7bb4363391cdc1d693f55535f3d42825d70
CyberBears <CyberBears8804@gmail.com>
2024-02-18 18:01:56 -0500

    backing up 2023 robot code after the season

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,173 +4,145 @@
 
 package frc.robot;
 
-//import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-//import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.*;
-
-//import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-//import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-
-
-//import edu.wpi.first.wpilibj.ADIS16470_IMU;
-//import edu.wpi.first.wpilibj.shuffleboard.*;
-
-
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 //IMPORTANT NOTE TO PROGRAMMER: press Fn and F1, type 'deploy robot code' and press enter in order to deploy the code.
-// Run the FRC Driver Station app in order to communicate with the RIO
-// Cross your fingers and move the joystick!
-// hope it works out, contact us if y'all need anything. Email me if you need at ansharyan03@gmail.com - Ansh, 4828 :D
+//Run the FRC Driver Station app in order to communicate with the RIO
+//Cross your fingers and move the joystick!
+//hope it works out, contact us if y'all need anything. Email me if you need at ansharyan03@gmail.com - Ansh, 4828 :D
 
-
-/**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
- * arcade steering.
- */
 public class Robot extends TimedRobot {
-  //left front = 11, left rear = 12, right front = 13, right rear = 14
+  // Left Front = 11, Left Rear = 12, Right Front = 13, Right Rear = 14
   private final CANSparkMax m_frontLeft = new CANSparkMax(11, MotorType.kBrushed);
   private final CANSparkMax m_rearLeft = new CANSparkMax(12, MotorType.kBrushed);
-  private final MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
-
   private final CANSparkMax m_frontRight = new CANSparkMax(13, MotorType.kBrushed);
   private final CANSparkMax m_rearRight = new CANSparkMax(14, MotorType.kBrushed);
+
+  private final MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
   private final MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
 
   private final DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
 
-  private final CANSparkMax m_Arm = new CANSparkMax(21, MotorType.kBrushless);
+  // Lower Shooter = 15, Upper Shooter = 16
+  private final CANSparkMax m_Shooter_Lower = new CANSparkMax(15, MotorType.kBrushed);
+  private final CANSparkMax m_Shooter_Upper = new CANSparkMax(16, MotorType.kBrushed);
 
-  //private final CANSparkMax m_shooter = new CANSparkMax(15, MotorType.kBrushed);
-
-  //private final VictorSPX intake = new VictorSPX(5);
   private final Joystick m_stick = new Joystick(0);
 
-  //private final ADIS16470_IMU gyro= new ADIS16470_IMU();
+  double robo_speed = .5;
+  double step = 0.1;
+  double speed_intake = 0.25;
+  double speed_launcher = -1;
+  double motor_speed = .5;
+  double timeElapsed = Timer.getFPGATimestamp();
 
-  double autoStart = 0.0;
-  
-  @Override
+  // Initializing the launch wheel
+  public void setLaunchWheel (double motor_speed) {
+    m_Shooter_Upper.set(motor_speed);
+  }
+
+  // Initializing the feed wheel
+  public void setFeedWheel (double motor_speed) {
+    m_Shooter_Lower.set(motor_speed);
+  }
+
+  // Initizaling the stop function
+  public void stop () {
+    m_Shooter_Lower.set(0);
+    m_Shooter_Upper.set(0);
+  }
+
+
+  @Overide
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
     m_left.setInverted(true);
     CameraServer.startAutomaticCapture();
-    
+
     // Places a compass indicator for the gyro heading on the dashboard
-    //Shuffleboard.getTab("gyro tab").add(gyro);
+    // Shuffleboard.getTab("gyro tab").add(gyro);
   }
-  
 
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_drive.arcadeDrive(-m_stick.getY()*0.7, -m_stick.getX()*0.79);
-    // check if the getY / X absolute value is at +- 0.7, make it a higher multiplier value, if not, a lower multiplier value
-    // xbox controller button A
-    if(m_stick.getRawButton(1)){
-      m_drive.arcadeDrive(-0.5, 0);
+    // Drive with arcade drive, which makes the Y-axis drive forward and the X-axis
+    // turn
+    m_drive.arcadeDrive(-m_stick.getY() * robo_speed, -m_stick.getX() * robo_speed);
+
+    // This method allows the driver to use the same joystick for high/low speed
+    // control and gives more precise control
+    // Reducing the speed with the A button but making sure it is still above 0
+    if (m_stick.getRawButton(2) && (robo_speed - step > 0)) {
+      robo_speed = robo_speed - step;
+      Timer.delay(0.1);
     }
-    // xbox controller button B
-    if(m_stick.getRawButton(2)){
-       m_drive.arcadeDrive(0.0, 0);
-    }
-        // xbox controller button X
-    if(m_stick.getRawButton(3)){
-      m_drive.arcadeDrive(0.0, 0);
-    }
-    // xbox controller button Y
-    if(m_stick.getRawButton(4)){
-      m_drive.arcadeDrive(0.5, 0);
+    // Increasing the speed with the B button but making sure it is below/equal to 1
+    else if (m_stick.getRawButton(3) && (robo_speed + step <= 1)) {
+      robo_speed = robo_speed + step;
+      Timer.delay(0.1);
     }
 
-    // xbox controller button Left Trigger / Rotate Left
-    if(m_stick.getRawButton(7)){
-      m_drive.arcadeDrive((0), 0.5);
-    }
-
-    // xbox controller button Right Trigger / Rotate Right
-    if(m_stick.getRawButton(8)){
-      m_drive.arcadeDrive(0, -.5);
-    }
-
-    // xbox controller axis Left trigger
-    if(m_stick.getRawAxis(2) != 0.0){
-      m_drive.arcadeDrive(0, m_stick.getY());
-    }
-    // xbox controller axis Right trigger
-    if(m_stick.getRawAxis(3) != 0.0){
-      m_drive.arcadeDrive(0, -.5);
-    }
-    // xbox controller button Left Bumper / Arm Down
-    if(m_stick.getRawButton(5)){
-      m_Arm.set(0.5);
-    }
-    // xbox controller button Right Bumpber / Arm Up
-    else if(m_stick.getRawButton(6)){
-      m_Arm.set(-0.5);
-    }
-    else {
-      m_Arm.set(0);
-    }
-  }
-
-/*
-    if(m_stick.getRawButton(4)){
-      intake.set(VictorSPXControlMode.PercentOutput, 0.3);
-    }
-    else {
-      intake.set(VictorSPXControlMode.PercentOutput, 0);
-    }
-  }
-/*
+    // Using the LB button to spin shooter motors in reverse to take in the note
     if (m_stick.getRawButton(5)) {
-      m_shooter.set(0.3);
+      m_Shooter_Upper.set(speed_intake);
+      m_Shooter_Lower.set(speed_intake);
     }
-    else {
-      m_shooter.set(0);
+    
+    // Using the RB button, spin up the upper shooter for 1 second, then spin up the
+    // lower shooter
+    else if (m_stick.getRawButton(6)) {
+      timeElapsed = Timer.getFPGATimestamp();
+      m_Shooter_Upper.set(speed_launcher);
+    }
+    else if (Timer.getFPGATimestamp() > (timeElapsed + 1) && m_stick.getRawButton(6)) {
+      m_Shooter_Lower.set(speed_launcher);
     }
 
-    if (m_stick.getRawButton(6)) {
-      m_shooter.set(-    .3);
-    }
-    else {
-      m_shooter.set(0);
-    }
-   */ 
+    //else if (Timer.getFPGATimestamp() > (timeElapsed + 2)) {
+     // m_Shooter_Upper.set(0);
+     // m_Shooter_Lower.set(0);
+    //}
 
-  public void autonomousInit() {
-    autoStart = Timer.getFPGATimestamp();
+    // Setting speed to 0 to stop motors once trigger is no longer pressed
+    else {
+      m_Shooter_Upper.set(0);
+      m_Shooter_Lower.set(0);
+    }
   }
 
+  // public void autonomousInit() {
+  // autoStart = Timer.getFPGATimestamp();
+  // }
 
+  /*
+   * (non-Javadoc)
+   * 
+   * @see edu.wpi.first.wpilibj.IterativeRobotBase#autonomousPeriodic()
+   */
   public void autonomousPeriodic() {
-    double timeElapsed = Timer.getFPGATimestamp() - autoStart;
-    // Drive forward X seconds
-    if (timeElapsed < 2) {
-      m_drive.arcadeDrive(0.3, 0);
+    double timeElapsed = Timer.getFPGATimestamp();
+    // Drive backwards X seconds
+    if (timeElapsed < 1) {
+      m_drive.arcadeDrive(-0.5, 0);
 
-      //intake controls 
+      // intake controls
+    } else if (timeElapsed < 5.1) {
+      m_drive.arcadeDrive(0.55, 0);
+
+      // intake controls
+    } else if (timeElapsed < 15) {
+      m_drive.arcadeDrive(.2, 0);
+
+      // intake controls
     }
-    // Then rotate for X seconds
-    else if (timeElapsed <4) {
-      m_drive.arcadeDrive(0,0.5 );
-    }
-    // Then drive again for X seconds
-    else if (timeElapsed <6) {
-      m_drive.arcadeDrive(0.3,0.5 );
-    }
-  
   }
 }
